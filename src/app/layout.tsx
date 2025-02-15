1c077f35c6f4a1065f5b4bcc6deb088b8cb13814
CarloDev15 <carlocaruso@outlook.com>
2025-02-15 02:13:01 +0100

    fix(main): 'fix' center style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({
   return (
     <html lang="it">
       <body className={helveticaRegular.className}>
-        <Navbar />
-        <div className="max-w-6xl mx-auto px-4 md:px-8">
+        <div className="max-w-7xl mx-auto px-4 md:px-8">
+          <Navbar />
           <main>{children}</main>
           <PopularLinks />
         </div>
