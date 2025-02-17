ae5848afe803e843cf2e211d66cc98080635df9d
Axster <andreacauchi@hotmail.it>
2025-02-17 18:30:27 +0100

    feat(main): add searchbar

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Category, CategoryKey, Filters, Product, ProductsResponse } from "./api.model";
 import { API_BASE_URL, categories } from "./api.utils";
-import { createQueryString } from "../../utils/crateQueryString";
+import { createQueryString } from "../../utils/createQueryString";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -24,7 +24,50 @@ export const getMenProducts = async (filters?: Filters) => {
   const responses = await Promise.all([
     api.get(`${categories["mens-shirts"].url}${query}`),
     api.get(`${categories["mens-shoes"].url}${query}`),
-    api.get(`${categories["mens-watches"].url}${query}`)
+    api.get(`${categories["mens-watches"].url}${query}`),
+  ]);
+
+  return responses.flatMap(response => response.data.products) as Product[];
+};
+
+export const getMenClothing = async (filters?: Filters) => {
+  const query = filters ? `?${createQueryString(filters)}` : "";
+  const responses = await Promise.all([
+    api.get(`${categories["mens-shirts"].url}${query}`),
+    api.get(`${categories["mens-shoes"].url}${query}`),
+  ]);
+  return responses.flatMap(response => response.data.products) as Product[];
+};
+
+export const getMenAccessories = async (filters?: Filters) => {
+  const query = filters ? `?${createQueryString(filters)}` : "";
+  const responses = await Promise.all([
+    api.get(`${categories["mens-watches"].url}${query}`),
+    api.get(`${categories["sunglasses"].url}${query}`),
+  ]);
+
+  return responses.flatMap(response => response.data.products) as Product[];
+};
+
+export const getWomenAccessories = async (filters?: Filters) => {
+  const query = filters ? `?${createQueryString(filters)}` : "";
+  const responses = await Promise.all([
+    api.get(`${categories["womens-bags"].url}${query}`),
+    api.get(`${categories["womens-jewellery"].url}${query}`),
+    api.get(`${categories["womens-watches"].url}${query}`),
+    api.get(`${categories["sunglasses"].url}${query}`),
+  ]);
+
+  return responses.flatMap(response => response.data.products) as Product[];
+};
+
+export const getWomenClothing = async (filters?: Filters) => {
+  const query = filters ? `?${createQueryString(filters)}` : "";
+  const responses = await Promise.all([
+    api.get(`${categories["womens-dresses"].url}${query}`),
+    api.get(`${categories["womens-shoes"].url}${query}`),
+    api.get(`${categories["tops"].url}${query}`),
+
   ]);
 
   return responses.flatMap(response => response.data.products) as Product[];
@@ -37,9 +80,20 @@ export const getWomenProducts = async (filters?: Filters) => {
     api.get(`${categories["womens-dresses"].url}${query}`),
     api.get(`${categories["womens-jewellery"].url}${query}`),
     api.get(`${categories["womens-shoes"].url}${query}`),
-    api.get(`${categories["womens-watches"].url}${query}`)
+    api.get(`${categories["womens-watches"].url}${query}`),
+    api.get(`${categories["tops"].url}${query}`),
   ]);
 
+  return responses.flatMap(response => response.data.products) as Product[];
+};
+
+export const getBeauty = async (filters?: Filters) => {
+  const query = filters ? `?${createQueryString(filters)}` : "";
+  const responses = await Promise.all([
+    api.get(`${categories["beauty"].url}${query}`),
+    api.get(`${categories["fragrances"].url}${query}`),
+    api.get(`${categories["skin-care"].url}${query}`),
+  ]);
   return responses.flatMap(response => response.data.products) as Product[];
 };
 
